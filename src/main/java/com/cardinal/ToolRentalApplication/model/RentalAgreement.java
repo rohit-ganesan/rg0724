912df91bdf2e7b68437606050fc829ff9b38cce6
Rohit Ganesan <rohit.k.ganesan@gmail.com>
2024-07-27 22:20:04 -0400

    added Tool and RentalAgreement model

---
+++ src/main/java/com/cardinal/ToolRentalApplication/model/RentalAgreement.java
@@ -0,0 +1,65 @@
+package com.cardinal.ToolRentalApplication.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+@Getter
+@Setter
+@ToString
+public class RentalAgreement {
+    private String toolCode;
+    private String toolType;
+    private String toolBrand;
+    private int rentalDays;
+    private LocalDate checkoutDate;
+    private LocalDate dueDate;
+    private double dailyRentalCharge;
+    private int chargeDays;
+    private double preDiscountCharge;
+    private int discountPercent;
+    private double discountAmount;
+    private double finalCharge;
+
+    public RentalAgreement(String toolCode, String toolType, String toolBrand, int rentalDays, LocalDate checkoutDate,
+                           LocalDate dueDate, double dailyRentalCharge, int chargeDays, double preDiscountCharge,
+                           int discountPercent, double discountAmount, double finalCharge) {
+        this.toolCode = toolCode;
+        this.toolType = toolType;
+        this.toolBrand = toolBrand;
+        this.rentalDays = rentalDays;
+        this.checkoutDate = checkoutDate;
+        this.dueDate = dueDate;
+        this.dailyRentalCharge = dailyRentalCharge;
+        this.chargeDays = chargeDays;
+        this.preDiscountCharge = preDiscountCharge;
+        this.discountPercent = discountPercent;
+        this.discountAmount = discountAmount;
+        this.finalCharge = finalCharge;
+    }
+
+    public void printAgreement() {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yy");
+        DecimalFormat currencyFormatter = new DecimalFormat("$#,##0.00");
+        DecimalFormat percentFormatter = new DecimalFormat("0%");
+
+        System.out.println("Tool code: " + toolCode);
+        System.out.println("Tool type: " + toolType);
+        System.out.println("Tool brand: " + toolBrand);
+        System.out.println("Rental days: " + rentalDays);
+        System.out.println("Check out date: " + checkoutDate.format(dateFormatter));
+        System.out.println("Due date: " + dueDate.format(dateFormatter));
+        System.out.println("Daily rental charge: " + currencyFormatter.format(dailyRentalCharge));
+        System.out.println("Charge days: " + chargeDays);
+        System.out.println("Pre-discount charge: " + currencyFormatter.format(preDiscountCharge));
+        System.out.println("Discount percent: " + percentFormatter.format(discountPercent / 100.0));
+        System.out.println("Discount amount: " + currencyFormatter.format(discountAmount));
+        System.out.println("Final charge: " + currencyFormatter.format(finalCharge));
+    }
+}
+
---
+++ src/main/java/com/cardinal/ToolRentalApplication/model/Tool.java
@@ -0,0 +1,28 @@
+package com.cardinal.ToolRentalApplication.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Tool {
+    private String code;
+    private String type;
+    private String brand;
+    private double dailyCharge;
+    private boolean weekdayCharge;
+    private boolean weekendCharge;
+    private boolean holidayCharge;
+
+    public Tool(String code, String type, String brand, double dailyCharge, boolean weekdayCharge, boolean weekendCharge, boolean holidayCharge) {
+        this.code = code;
+        this.type = type;
+        this.brand = brand;
+        this.dailyCharge = dailyCharge;
+        this.weekdayCharge = weekdayCharge;
+        this.weekendCharge = weekendCharge;
+        this.holidayCharge = holidayCharge;
+    }
+}
